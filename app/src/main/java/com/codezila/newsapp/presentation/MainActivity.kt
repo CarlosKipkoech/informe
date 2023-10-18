465b992982821d231ecc1598f6905246d9ff5cae
CarlosKipkoech <carlos598798@gmail.com>
2023-10-18 17:37:58 +0300

    make system bars transparent

--- app/src/main/java/com/codezila/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/codezila/newsapp/presentation/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -16,6 +19,7 @@ import com.codezila.newsapp.presentation.navgraph.NavGraph
 import com.codezila.newsapp.presentation.onboarding.OnBoardingScreen
 import com.codezila.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.codezila.newsapp.ui.theme.NewsAppTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -35,6 +39,16 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             NewsAppTheme {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
                     NavGraph(startDestination = viewModel.startDestination)
                 }
